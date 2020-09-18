1f0225ce6aab463bc0b067b074a22d0068175ffc
yogesh-112 <512yogesh-112@users.noreply.github.com>
2020-09-18 19:05:35 +0530

    we are a router company
    we create routers

---
+++ header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>4ipv4</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/dist/favicon.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Multi - v2.1.0
+  * Template URL: https://bootstrapmade.com/multi-responsive-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <img src="assets/img/dist/logo.png" height="60" width="60">
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block" style="text-align: right;">
+        <ul>
+          <li class="active; drop-down"><a href="index.html">Network Consultancy Services</a>
+            <ul>
+              <li><a href="#">Network Installations</a></li>
+              <li><a href="#">Maintenance Support</a></li>
+              <li><a href="#">Network Optimization</a></li>
+              <li><a href="#">NOC Operational Consulting</a></li>
+              <li><a href="#">Network Design</a></li>
+              <li><a href="#">All Services</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#about">On Demand NOC Support</a>
+            <ul>
+              <li><a href="#">Comprehensive Site Surveys</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#services">Additional Services</a>
+            <ul>
+              <li><a href="#">SOC Services</a></li>
+              <li><a href="#">NOC Services</a></li>
+              <li><a href="#">Hosting Solution</a></li>
+              <li><a href="#">Backup Solutions</a></li>
+              <li><a href="#">Migration Services</a></li>
+              <li><a href="#">Virtualization Services</a></li>
+              <li><a href="#">PANEL Support</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#portfolio">Company</a>
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">BCP</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#team">Resources</a>
+            <ul>
+              <li><a href="#">Learning Center</a></li>
+              <li><a href="#">Tools</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">News/Articles</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Consult Us</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
